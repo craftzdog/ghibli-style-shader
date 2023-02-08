8fe941ce3d8e7c572df14f84013c752e8acff638
Takuya Matsuyama <hi@craftz.dog>
2023-02-08 22:54:10 +0900

    fix(shader): hack ToonShader

--- src/ToonShader.js
+++ src/ToonShader.js
@@ -48,12 +48,20 @@ export const ToonShader = {
       float camera = max( dot( normalize( vNormal ), vec3( 0.0, 0.0, 1.0 ) ), 0.4);
       float light = max( dot( normalize( vNormal ), uDirLightPos ), 0.0);
       gl_FragColor = vec4( uBaseColor, 1.0 );
-
-      if ( length(uAmbientLightColor + uDirLightColor * light) < 1.00 ) {
+      gl_FragColor = vec4(0.082, 0.212, 0.192, 1.0);
+      if ( length(light) > 0.0 ) {
+        gl_FragColor = vec4(0.094, 0.278, 0.29, 1.0);
+      }
+      if ( length(light) > 10.99 ) {
+        gl_FragColor = vec4(0.2, 0.459, 0.376, 1.0);
+      }
+      if ( length(light) > 20.99 ) {
         gl_FragColor *= vec4( uLineColor1, 1.0 );
+        gl_FragColor = vec4(0.286, 0.537, 0.486, 1.0);
       }
       if ( length(uAmbientLightColor + uDirLightColor * camera) < 0.50 ) {
         gl_FragColor *= vec4( uLineColor2, 1.0 );
+        gl_FragColor = vec4(1.0, 1.0, 0.0, 0.0);
       }
     }
   `,
