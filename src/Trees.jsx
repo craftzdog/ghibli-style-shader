80df5f286d9b0189ba83f22eb85e8d3b56b2900a
Takuya Matsuyama <hi@craftz.dog>
2023-02-08 22:34:00 +0900

    fix(trees): use standard material

--- src/Trees.jsx
+++ src/Trees.jsx
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
+import { Color } from "three";
 
 export function Trees(props) {
   const { nodes, materials } = useGLTF("/trees.glb");
@@ -9,9 +10,12 @@ export function Trees(props) {
         castShadow
         receiveShadow
         geometry={nodes.Foliage.geometry}
-        material={materials["Stylized Foliage"]}
         position={[0.33, -0.05, -0.68]}
-      />
+      >
+        <meshStandardMaterial
+          color={new Color("#33594e").convertLinearToSRGB()}
+        />
+      </mesh>
     </group>
   );
 }
