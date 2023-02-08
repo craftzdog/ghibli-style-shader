9350c16ec340f222b24fdc81f24d6233cd8b0489
Takuya Matsuyama <hi@craftz.dog>
2023-02-08 22:36:07 +0900

    fix(shader): try gradientMap

--- src/Trees.jsx
+++ src/Trees.jsx
@@ -1,9 +1,21 @@
-import { useRef } from "react";
+import { useRef, useMemo } from "react";
 import { useGLTF } from "@react-three/drei";
-import { Color } from "three";
+import { RedFormat, DataTexture } from "three";
 
 export function Trees(props) {
-  const { nodes, materials } = useGLTF("/trees.glb");
+  const { nodes } = useGLTF("/trees.glb");
+
+  const toneMap = useMemo(() => {
+    const format = RedFormat;
+    const colors = new Uint8Array(4);
+    for (let c = 0; c <= colors.length; c++) {
+      colors[c] = (c / colors.length) * 256;
+    }
+    const gradientMap = new DataTexture(colors, colors.length, 1, format);
+    gradientMap.needsUpdate = true;
+    return gradientMap;
+  }, []);
+
   return (
     <group {...props} dispose={null}>
       <mesh
@@ -12,7 +24,7 @@ export function Trees(props) {
         geometry={nodes.Foliage.geometry}
         position={[0.33, -0.05, -0.68]}
       >
-        <meshToonMaterial color="#234549" />
+        <meshToonMaterial gradientMap={toneMap} color="#234549" />
       </mesh>
     </group>
   );
